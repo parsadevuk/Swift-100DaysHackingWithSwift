6508139d479dd6a6377daabfa2766a69861578a8
Parsa Shobany <Parsa_sh@hotmail.co.uk>
2022-09-12 10:45:34 +0100

    Hi

---
+++ HackingWithSwift-003.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Created by Parsa Shobany on 2022-09-12
+
+import UIKit
+
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+let reminder = 13 % secondScore
