9818c7596cceb3d16d301ab84570086dfb93186b
Parsa Dev <parsa.dev.uk@gmail.com>
2022-11-17 10:56:45 +0000

    Day 7

--- HackingWithSwift-006.playground/Contents.swift
+++ HackingWithSwift-006.playground/Contents.swift
@@ -1,6 +1,38 @@
 import UIKit
 
-let driving = {
+//let driving = {
+//    print("I'm driving in my car")
+//}
+
+//let driving = { (place: String) in
+//    print("I'm going to \(place) in my car")
+//}
+//driving("London")
+
+//let drivingWithReturn = { (place: String) -> String in
+//
+//    return "I'm going to \(place) in my car"
+//}
+//
+//
+//print(drivingWithReturn("London"))
+//
+//let driving = {
+//    print("I'm driving in my car")
+//}
+//func travel(action: () -> Void){
+//    print("I'm getting ready to go.")
+//    action()
+//    print("I arrived!")
+//}
+//travel(action: driving)
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()// you can run a code later here.
+    print("I arrived!")
+}
+
+travel (){
     print("I'm driving in my car")
 }
-driving()
