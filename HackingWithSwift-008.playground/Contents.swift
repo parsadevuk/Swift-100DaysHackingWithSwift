24891a516f8e4d0d39ca7abad06f71055a0f2d89
Parsa Dev <parsa.dev.uk@gmail.com>
2023-01-31 14:51:01 +0000

    Day 09

--- HackingWithSwift-008.playground/Contents.swift
+++ HackingWithSwift-008.playground/Contents.swift
@@ -44,4 +44,29 @@ progress.amount = 100
 //Mutating methods
 
 ///--Mutating methods--
-struct
+struct Person {
+    var name: String
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+
+///Properties and methods of strings
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+///Properties and methods of arrays
+
+var toys = ["Woody"]
+print(toys.count)
+
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print("print the toys: \(toys.indices)")
+print(toys.sorted())
