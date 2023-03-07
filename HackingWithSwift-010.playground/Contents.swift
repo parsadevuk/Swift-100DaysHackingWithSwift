b10accbaf5e763015593acfb8457edea3c529507
Parsa Dev <parsa.dev.uk@gmail.com>
2023-03-07 12:44:47 +0000

    Day 10

---
+++ HackingWithSwift-010.playground/Contents.swift
@@ -0,0 +1,12 @@
+//  Created by Parsa Dev on 07/03/2023.
+import UIKit
+
+class Dog {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
